62675b21c6c4c1428dc95cc4269061556f1c61f7
fgNv <felipe.underlost@gmail.com>
2017-12-04 00:13:12 -0200

    heroku deploy

--- server.js
+++ server.js
@@ -13,9 +13,11 @@ const forceSSL = function () {
     }
 }
 app.use(forceSSL());
+
+app.use(express.static(__dirname + '/dist'));
+
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
